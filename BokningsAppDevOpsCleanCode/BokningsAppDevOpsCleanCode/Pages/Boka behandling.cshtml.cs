b9042df6d2b9a18a55fc1545ed798265352774be
Emma Persson <essbp96@hotmail.com>
2023-11-27 10:17:14 +0100

    calander updates days and month and year

--- BokningsAppDevOpsCleanCode/BokningsAppDevOpsCleanCode/Pages/Boka behandling.cshtml.cs
+++ BokningsAppDevOpsCleanCode/BokningsAppDevOpsCleanCode/Pages/Boka behandling.cshtml.cs
@@ -1,29 +1,82 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 using System.Globalization;
 
 namespace BokningsAppDevOpsCleanCode.Pages
 {
     public class Boka_behandlingModel : PageModel
     {
+        public int CurrentYear { get; set; }
+        public int CurrentMonth { get; set; }
+        public List<List<int>> CalendarWeeks { get; set; }
 
-        public int CurrentYear { get; private set; }
-        public int CurrentMonth { get; private set; }
-        public List<List<int>> CalendarWeeks { get; private set; }
-        public string MonthInLetters { get; private set; }
-        public void OnGet()
+        public IActionResult OnGet(int year, int month)
         {
-            // Set the current year and month
-            CurrentYear = DateTime.Now.Year;
-            CurrentMonth = DateTime.Now.Month;
-            MonthInLetters = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(CurrentMonth));
+            // If the provided year and month are valid, use them; otherwise, use the current date
+            if (IsValidYearMonth(year, month))
+            {
+                CurrentYear = year;
+                CurrentMonth = month;
+            }
+            else
+            {
+                var currentDate = DateTime.Now;
+                CurrentYear = currentDate.Year;
+                CurrentMonth = currentDate.Month;
+            }
 
+            // Update the calendar
+            UpdateCalendar();
 
-            // Generate the calendar weeks
-            CalendarWeeks = GenerateCalendarWeeks(CurrentYear, CurrentMonth);
+            return Page();
         }
 
-        private List<List<int>> GenerateCalendarWeeks(int year, int month)
+        public IActionResult OnGetNextMonth()
+        {
+            // Update the calendar for the next month
+            CurrentMonth++;
+            if (CurrentMonth > 12)
+            {
+                CurrentMonth = 1;
+                CurrentYear++;
+            }
+
+            UpdateCalendar();
+
+            // Redirect to the current page with updated parameters
+            return RedirectToPage("Boka_behandling", new { year = CurrentYear, month = CurrentMonth });
+        }
+
+        public IActionResult OnGetPreviousMonth()
+        {
+            // Update the calendar for the previous month
+            CurrentMonth--;
+            if (CurrentMonth < 1)
+            {
+                CurrentMonth = 12;
+                CurrentYear--;
+            }
+
+            UpdateCalendar();
+
+            // Redirect to the current page with updated parameters
+            return RedirectToPage("Boka_behandling", new { year = CurrentYear, month = CurrentMonth });
+        }
+
+        private void UpdateCalendar()
+        {
+            // Generate the calendar for the current year and month
+            CalendarWeeks = GenerateCalendar(CurrentYear, CurrentMonth);
+        }
+
+        private bool IsValidYearMonth(int year, int month)
+        {
+            return year >= 1 && month >= 1 && month <= 12;
+        }
+
+
+        private List<List<int>> GenerateCalendar(int year, int month)
         {
             List<List<int>> weeks = new List<List<int>>();
             DateTime firstDayOfMonth = new DateTime(year, month, 1);
@@ -53,4 +106,5 @@ namespace BokningsAppDevOpsCleanCode.Pages
             return weeks;
         }
     }
+
 }
