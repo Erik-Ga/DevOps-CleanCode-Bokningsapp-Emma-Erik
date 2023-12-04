5c3524a19568e80e6f85329a5898e03c30a1d070
Emma Persson <essbp96@hotmail.com>
2023-12-04 12:03:59 +0100

    Added 6 Unit Tests

--- BokningsAppDevOpsCleanCode/BokningsAppDevOpsCleanCode.UnitTests/BookingServiceTests.cs
+++ BokningsAppDevOpsCleanCode/BokningsAppDevOpsCleanCode.UnitTests/BookingServiceTests.cs
@@ -7,23 +7,6 @@ namespace BokningsAppDevOpsCleanCode.UnitTests
 {
     public class BookingServiceTests
     {
-        //[Fact]
-        //public void AddBooking_ShouldAddBookingToDatabase()
-        //{
-        //    // Arrange
-        //    var bookingService = new BookingService();
-        //    var bookingToAdd = new Booking { /* initialize properties */ };
-
-        //    // Act
-        //    bookingService.AddBooking(bookingToAdd);
-
-        //    // Assert
-        //    // You would need to query the database or mock the database to verify the booking was added successfully.
-        //    // This could involve checking the count of bookings before and after the addition, or querying the database for the specific booking.
-        //    // For simplicity, let's assume an assertion like this:
-        //    Assert.True(/* some condition indicating the booking was added */);
-        //}
-
         [Fact]
         public void GetBookingsFromUserWithUserId_ShouldReturnUserHasBooking()
         {
@@ -52,23 +35,25 @@ namespace BokningsAppDevOpsCleanCode.UnitTests
             var actual = booking.ChosenTreatment;
 
             // Assert
-            Assert.Equal(expected,actual);
+            Assert.Equal(expected, actual);
             // You might have more specific assertions based on the expected behavior of the method.
         }
-
-        //[Fact]
-        //public void CancelBooking_ShouldRemoveBookingFromDatabase()
-        //{
-        //    // Arrange
-        //    var bookingService = new BookingService(_dbContext);
-        //    var bookingToCancel = new Booking { /* initialize properties */ };
-
-        //    // Act
-        //    bookingService.CancelBooking(bookingToCancel);
-
-        //    // Assert
-        //    // Similar to AddBooking, you would need to verify the booking was removed from the database.
-        //    Assert.True(/* some condition indicating the booking was canceled */);
-        //}
+        [Theory]
+        [InlineData("emma@hotmail.com")]
+        [InlineData("erik@hotmail.com")]
+        [InlineData("tezla@hotmail.com")]
+        [InlineData("sealis@hotmail.com")]
+        [InlineData("cheato@hotmail.com")]
+        [InlineData("drakis@hotmail.com")]
+        public void CheckIfCustomersByUserIdHasBookings_ShouldReturnTrue(string userId)
+        {
+            //Arrange
+            var bookingService = new MockDatabase();
+            //Act
+            var expected = true;
+            var actual = bookingService.GetAndCheckIfCustomersHasBookings(userId);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
     }
 }
